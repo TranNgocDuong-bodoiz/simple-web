2fe97994527543b139c1d9c19de6fde58025125f
LongNger <baolong372004@gmail.com>
2024-10-20 16:24:22 +0700

    gio-hang

---
+++ pages/index/main/giohang.php
@@ -0,0 +1,92 @@
+<div class="page_header">
+        <!-- 1.logo / 2.danh_muc/ 3.search_bar / 4.hotline/ 5.tra_cuu_don_hang/ 6.gio_hang/ 7.tai_khoan -->
+        <div class="logo">
+            <div class="shop_name">
+                <span>SIMPLE MOBILE</span>
+            </div>
+            <div class="shop_content">
+                favourite online store
+            </div>
+        </div>
+        <div class="danh_muc_container">
+            <div class="danh_muc">
+                <i class="fa-solid fa-bars"></i>
+                <span>Danh mục</span>
+            </div>
+            <ul class="danh_muc_list">
+                <li class="gap_item"><i class="fa-solid fa-angle-down"></i></li>
+                <li class="danh_muc_item">
+                    <i class="fa-regular fa-tablet"></i>
+                    <a href="index.php">Trang chủ</a>
+                </li>
+                <li class="danh_muc_item">
+                    <i class="fa-solid fa-mobile-screen-button"></i>
+                    <a href="index.php?quanly=dienthoai">Điện thoại</a>
+                </li>
+                
+                <li class="danh_muc_item">
+                     <i class="fa-regular fa-tablet"></i>
+                    <a href="index.php?quanly=tablet">Tablet</a>
+                </li>
+                <li class="danh_muc_item">
+                    <i class="fa-solid fa-headphones"></i>
+                    <a href="index.php?quanly=phukien">Phụ kiện</a>
+                </li>
+                <li class="danh_muc_item">
+                    <i class="fa-regular fa-newspaper"></i>
+                    <a href="index.php?quanly=tintuc">Tin tức</a>
+                </li>
+            </ul>
+        </div>
+        <div class="search_bar">
+            <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            <input type="text" placeholder="Hôm nay bạn cần tìm gì" autocomplete="off">
+        </div>
+        <div class="hotline">
+            <a href="#">
+                <div class="hotline_icon"><i class="fa-solid fa-phone-volume"></i></div>
+                <div class="hotline_infor">
+                    <span id="hotline_text">Gọi mua ngay</span>
+                    <span id="hotline_number">0888888889</span>
+                </div>
+            </a>
+        </div>
+        <div class="gio_hang">
+            <a href="#">
+                <div class="gio_hang_icon"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div class="gio_hang_infor"><span id="gio_hang_text">giỏ hàng</span></div>
+            </a>
+        </div>
+        <div class="tai_khoan">
+            <a href="#">
+                <div class="tai_khoan_ct">
+                    <i class="fa-regular fa-user"></i>
+                    <span>Tài khoản</span>
+            </div>
+            
+            </a>
+        </div>
+    </div> <!-- Đóng thẻ page_header -->
+
+
+    
+
+
+    <!-- footer --> 
+<div class="footer">
+        <div class="footer-logo"><img src="imgaes/logo2.png" height="100%" alt=""></div>
+            <div class="text text--name">Cửa hàng điện thoại simple mobile</div>
+            <div class="text-container-a">
+                <div class="text text--desc">Địa chỉ:</div>
+                <div class="text text--infor">Số 41A đường Phú Diễn, phường Phú Diễn, quận Bắc Từ Liêm, thành phố Hà Nội.</div>
+            </div>
+            <div class="text-container-p">
+                <div class="text text--desc">Điện thoại:</div>
+                <div class="text text--infor">0888888999</div>
+            </div>
+            <div class="tex-container-e">
+            <div class="text text--desc">Email:</div>
+            <div class="text text--infor">simplemobile.bodoi.com</div>
+            </div>
+    </div>
+    <!-- footer --> 
