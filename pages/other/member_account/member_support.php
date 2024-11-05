2818d9247c824e23bb91a1b07d9cea5aa04ddc49
TranNgocDuong-bodoiz <22111061237@hunre.edu.vn>
2024-11-06 01:02:23 +0700

    update

--- pages/other/member_account/member_support.php
+++ pages/other/member_account/member_support.php
@@ -1,11 +1,15 @@
 <div class="member-bar">
             <div class="support-member">
-                <div class="title" style="text-align: center;font-size: 20px;font-weight: 800;color:red;
-                                        padding:20px;">
+                <div class="title" >
                     Muốn hỗ trợ hãy tìm đến dương bodoi
                 </div>
                 <div class="support-box">số điện thoại: 0818930855</div>
+                <div class="support-box">email: ngocrong.5102004@gmail.com</div>
                 <div class="support-box">luôn sẵn sàng giúp đỡ</div>
+                <div class="cart">
+                    <span>Hòm Công Đức</span>
+                    <img src="./myqr.jpg" alt="" width="100%">
+                </div>
             </div>
              
         </div>
